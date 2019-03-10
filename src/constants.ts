4f41e539fbc7e159c3580eedb9102a6337937e7f
Mike Smith <mike@noiseparticles.com>
2019-03-10 11:36:23 -0400

    tweaking monster stats

--- src/constants.ts
+++ src/constants.ts
@@ -2,16 +2,18 @@ import { MonsterType } from "./classes/monsterType";
 import { Item } from "./classes/item";
 
 export const monsterTypes = [
+    // hp, str, armor
     new MonsterType(1,1,'Bat','~',1,2,0,'#f00'),
     new MonsterType(1,1,'Bat','~',1,1,0,'#f00'),
-    new MonsterType(2,3,'Manbat','B',2,2,0,'#f00'),
+    new MonsterType(2,3,'Manbat','B',3,3,0,'#f00'),
     new MonsterType(2,3,'Minotaur','m',3,3,1,'#f00'),
     new MonsterType(3,4,'Super Minotaur','M',4,4,2,'#f00'),
     new MonsterType(2,3,'Wizard','W',2,2,0,'#f00'),
     new MonsterType(1,1,'Snake','s',1,1,0,'#f00'),
     new MonsterType(1,2,'Rat','r',1,2,0,'#f00'),
-    new MonsterType(2,4,'Wyvern','Y',2,1,1,'#f00'),
-    new MonsterType(2,4,'Wyvern','Y',2,1,1,'#f00'),
+    new MonsterType(2,4,'Wyvern','Y',2,2,1,'#f00'),
+    new MonsterType(2,4,'Wyvern','Y',2,2,1,'#f00'),
+    new MonsterType(3,4,'Wyvern','Y',3,3,1,'#f00'),
 ];
 
 export const items = [
